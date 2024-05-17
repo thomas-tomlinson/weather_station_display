8717df95e3131d70cc3c82573b78abf93bbf0d61
Thomas Tomlinson <thomas_tomlinson@mac.com>
2024-05-17 16:47:56 -0600

    graphing data based on parsing the belchertown weewx data

--- panels/graph.py
+++ panels/graph.py
@@ -4,25 +4,62 @@ from PyQt6.QtGui import QPainter, QPolygon, QFont, QFontMetrics, QPen, QColor
 from PyQt6.QtWidgets import QApplication
 import pyqtgraph as pg 
 import sys
-from random import randint
+import requests
+import json
 
 class Graph(QtWidgets.QMainWindow):
-
+    _data = {}
     def __init__(self, *args, **kwargs):
         super(Graph, self).__init__(*args, **kwargs)
-
+        self._data = []
+        self._data_view_current = 0
         self.plot_graph = pg.PlotWidget()
         self.setCentralWidget(self.plot_graph) 
-        self.minutes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        self.temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 30]
+
+        self.fetch_data()
+        self.draw_graph()
+
+    def draw_graph(self):
+        series = self._data_view_current
         pen = pg.mkPen(color=(255, 0, 0))
-        self.plot_graph.setLabel("left", "Temp")
+        self.plot_graph.setLabel("left", self._data[series]['y_axis_label'])
         self.plot_graph.setLabel("bottom", "Time")
-        self.graph = self.plot_graph.plot(self.minutes, self.temperature, pen=pen)
+        self.graph = self.plot_graph.plot(self._data[series]['x_axis'], self._data[series]['y_axis'], pen=pen)
 
     def mousePressEvent(self, event):
-        pen = pg.mkPen(color=(randint(0, 255), randint(0, 255), randint(0, 255)))
-        self.graph.setData(self.minutes, self.temperature, pen=pen)
+        # increment our view counter
+        if self._data_view_current  == len(self._data) - 1:
+            self._data_view_current = 0
+        else:
+            self._data_view_current += 1 
+        self.graph.clear()
+        self.draw_graph()
+
+    def fetch_data(self):
+        holder = []
+        # remove all data
+        self._data = []
+        raw_data = requests.get('http://wdisplay01.local/weewx/belchertown/json/day.json')
+        json_data = json.loads(raw_data.content)        
+        for keys in json_data:
+            if keys.startswith('chart') is True and isinstance(json_data[keys], dict):
+                holder.append(json_data[keys])
+
+        for entry in holder:
+            for charttype in entry['series']:
+                temp_object = {}
+                temp_object['name'] = entry['series'][charttype]['name']
+                temp_object['y_axis_label'] = entry['series'][charttype]['yAxis_label']
+                temp_object['x_axis'] = []
+                temp_object['y_axis'] = []
+                for value in entry['series'][charttype]['data']:
+                    temp_object['x_axis'].append(value[0])
+                    temp_object['y_axis'].append(value[1])
+                self._data.append(temp_object)
+
+
+
+
 
 
 if __name__ == '__main__':
