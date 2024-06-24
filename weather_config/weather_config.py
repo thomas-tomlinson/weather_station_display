17aa54afe52b544ebc06c51a7ea5db372e26ea6c
Thomas Tomlinson <thomas_tomlinson@mac.com>
2024-06-24 14:28:18 -0600

    moved config to json, stupid python module hell

--- weather_config/weather_config.py
+++ weather_config/weather_config.py
@@ -1,14 +1,32 @@
-import config
+import json
 
+cfg = {}
 def default_config():
     #sane defaults
-    config = {}
-    config['weewx_host'] = 'weewx.local'
-    config['sat_images'] = ['https://cdn.star.nesdis.noaa.gov/GOES18/ABI/SECTOR/pnw/GEOCOLOR/300x300.jpg']
-    return config
+    def_cfg = {}
+    def_cfg['weewx_host'] = 'weewx.local'
+    def_cfg['sat_images'] = ['https://cdn.star.nesdis.noaa.gov/GOES18/ABI/SECTOR/pnw/GEOCOLOR/300x300.jpg']
+    return def_cfg
 
-try:
-    cfg = config.Config('weather.cfg') 
-except FileNotFoundError as e:
-    # config file wasn't found
+def load_config():
+    json_config = {}
+    try:
+        f = open('weather_cfg.json')
+        
+    except FileNotFoundError as e:
+        return
+
+    try:
+        json_config = json.load(f)
+        return json_config
+    except Exception as e:
+        return
+
+
+config_file = load_config()
+if config_file is not None:
+    cfg = config_file
+else:
     cfg = default_config()
+
+
